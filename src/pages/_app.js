223fd3f3bb25e8dd0218ee90ef845adcc32bb374
Dylan Finney <dylan.finney@outlook.com>
2023-03-13 13:13:03 +0000

    GTM

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,17 @@
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/provider';
 import {theme} from "../components/theme"
+import TagManager from "react-gtm-module"
+import { useEffect } from 'react';
+import { gtm } from 'react-gtm-module/dist/TagManager';
 function MyApp({ Component, pageProps }) {
+  const gtmId = process.env.NEXT_PUBLIC_GTM || "";
+  const tagManagerArgs = {
+    gtmId
+  }
+  useEffect(()=>{
+    TagManager.initialize(tagManagerArgs)
+  },[])
   return (
     <ChakraProvider theme={theme}>
         <Component {...pageProps} />
