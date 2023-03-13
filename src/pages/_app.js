fb6b32736aaf2ee652a3fccd854e8976bdb46f27
Dylan Finney <dylan.finney@outlook.com>
2023-03-13 13:42:01 +0000

    Google Analytics Fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,33 @@
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/provider';
 import {theme} from "../components/theme"
-import TagManager from "react-gtm-module"
+import Script from 'next/script'
 import { useEffect } from 'react';
-import { gtm } from 'react-gtm-module/dist/TagManager';
+
 function MyApp({ Component, pageProps }) {
   const gtmId = process.env.NEXT_PUBLIC_GTM || "";
-  const tagManagerArgs = {
-    gtmId
-  }
-  useEffect(()=>{
-    TagManager.initialize(tagManagerArgs)
-  },[])
+  
   return (
+    <>
+    <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gtmId}`}/>
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${gtmId}', {
+          page_path: window.location.pathname,
+        });
+        `,
+      }}
+    />
     <ChakraProvider theme={theme}>
         <Component {...pageProps} />
     </ChakraProvider>
+    </>
   )
 }
 
